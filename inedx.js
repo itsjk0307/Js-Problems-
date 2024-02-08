eaade5ce17a02d86989f667dea779e3f17d6a5b9
Jamshid Khaytbaev <itsjk0307@gmail.com>
2024-02-09 00:04:54 +0900

    '.'

--- inedx.js
+++ inedx.js
@@ -1,7 +1,24 @@
 // 1. Problem: Print Numbers from 1 to n
 //  Write a program that takes an input n from the user and prints all the numbers from 1 to n on separate lines.
 
-var n = 10;
-for (let i = 1; i <= n; i++) {
-  console.log(i);
+// var n = 10;
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+// Write a function that takes two numbers as arguments and returns their sum.
+
+function sumFunc(a, b) {
+  return a + b;
 }
+console.log(sumFunc(5, 3));
+
+// Write a function that takes two numbers as arguments and returns their product.
+
+function multipy(a, b) {
+  var product = a * b;
+  return product;
+}
+
+var result = multipy(5, 5);
+console.log(result);
